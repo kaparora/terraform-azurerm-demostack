4e5c9ef70184256cef0da6839168a9c154998003
Kapil Arora <512729+kaparora@users.noreply.github.com>
2020-06-28 17:15:43 +0200

    palcement error

--- modules/azurekeys.tf
+++ modules/azurekeys.tf
@@ -86,6 +86,6 @@ resource "azurerm_key_vault_key" "demostack" {
     TTL       = var.TTL
     owner     = var.owner
     demostack = var.consul_join_tag_value
-  depends_on = [azurerm_key_vault_access_policy.demostack_vm]
 }
+  depends_on = [azurerm_key_vault_access_policy.demostack_vm]
 }
